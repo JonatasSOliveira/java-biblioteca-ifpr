97e085324a97752d91ac073b3edcd0fd2d742df7
santyero <santyero@gmail.com>
2022-07-30 12:48:15 -0300

    Implementação de getters and setters e construtores

--- src/main/java/modelos/Pessoa.java
+++ src/main/java/modelos/Pessoa.java
@@ -20,5 +20,80 @@ public class Pessoa {
     private Date dataAtualizacao;
     private Date dataCriacao;
 
-}
+    public Pessoa() {
+    }
 
+    public Pessoa(String nome, String senha, Date dataCriacao) {
+        this.nome = nome;
+        this.senha = senha;
+        this.dataCriacao = dataCriacao;
+    }
+    
+    
+    public Pessoa(String nome, String email, String telefone, String senha, boolean ativo, Date dataAtualizacao, Date dataCriacao) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.senha = senha;
+        this.ativo = ativo;
+        this.dataAtualizacao = dataAtualizacao;
+        this.dataCriacao = dataCriacao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public Date getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(Date dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+}
